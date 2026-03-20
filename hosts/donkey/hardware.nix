4e75055a0f7bc3dcbce2cb520f6cf054b6a0b627
jadendino <jadendino@berkeley.edu>
2026-03-20 01:13:33 -0700

    donkey: disable sleep

--- hosts/donkey/hardware.nix
+++ hosts/donkey/hardware.nix
@@ -1,3 +1,16 @@
 {
   nixpkgs.hostPlatform = "aarch64-darwin";
+
+  system.pmset.all = {
+    sleep = 0;
+    displaysleep = 0;
+    disksleep = 0;
+    standby = 0;
+    autopoweroff = 0;
+    hibernatemode = 0;
+    powernap = 0;
+    tcpkeepalive = 0;
+    womp = 1;
+    autorestart = 1;
+  };
 }
